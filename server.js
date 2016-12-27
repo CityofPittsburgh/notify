6e111fc033e6fbfe7deab0ce2889aaee1fc43aa9
Nick Hall <nick@inck.net>
2016-12-27 17:24:47 -0500

    Validated requests and clarified responses.
    And cleaned up a little in the process.

--- server.js
+++ server.js
@@ -1,60 +1,75 @@
 var restify = require('restify'),
 	request = require('request'),
-	account = process.env.TWILIO_ACCOUNT,
-	number = process.env.TWILIO_NUMBER,
-	token = process.env.TWILIO_TOKEN,
-	baseUrl = "https://api.twilio.com/2010-04-01/",
-	notificationUrl = baseUrl + 'Accounts/' + account + '/Messages.json',
+	twilioUrl = "https://api.twilio.com/2010-04-01/",
+	twilioAccount = process.env.TWILIO_ACCOUNT,
+	twilioNumber = process.env.TWILIO_NUMBER,
+	twilioToken = process.env.TWILIO_TOKEN,
+	notifyToken = process.env.NOTIFY_TOKEN,
 	notifications = {
 		"snow": "Calling all Snow Angels! Can you shovel today?"
 	};
 
 // Handle notification requests.
 function notify(req, res, next) {
+
+	var notification = notifications[req.params.notification]
+
+	// Check input.
 	if(!req.body) {
-		res.send(400);
+		res.send(400, { "message": "Request body is empty." });
+		next();
+	} else 
+
+	// Check auth.
+	if(req.query["token"] == undefined || req.query["token"] != notifyToken) {
+		res.send(403, { "message": "Notification requires authentication." });
+		next();
+	} else {
+
+		// For each number.
+		for(var property in req.body) {
+
+			// Standardize recipient number.  (Remove non-numbers.)  (Remove initial '1' if present.)
+			var recipient = req.body[property].replace(/[^0-9]/g, "").replace(/^1(.*)/g, "$1");
+			console.log("Queueing message to " + recipient);
+
+			// Queue message.
+			request.post(
+				{
+					auth: {
+						"user": twilioAccount,
+						"pass": twilioToken
+					},
+					url: twilioUrl + 'Accounts/' + twilioAccount + '/Messages.json',
+					form: {
+						"To": "+1" + recipient,
+						"From": twilioNumber,
+						"Body": notification
+					}
+				},
+				function(error, httpResponse, body) {
+					if(error) { 
+						console.log(error);
+					} else if(body) { 
+						console.log(body);
+					}
+				}
+			);
+		}
+		res.send(201, { 
+			"notification": notification,
+			"numbers": req.body
+		});
 		next();
 	}
-
-	// For each number.
-	for(var property in req.body) {
-
-		// Standardize recipient number.  (Remove non-numbers.)  (Remove initial '1' if present.)
-		var recipient = req.body[property].replace(/[^0-9]/g, "").replace(/^1(.*)/g, "$1");
-		console.log("Queueing message to " + recipient);
-
-		// Queue message.
-		request.post(
-			{
-				auth: {
-					"user": account,
-					"pass": token
-				},
-				url: notificationUrl,
-				form: {
-					"To": "+1" + recipient,
-					"From": number,
-					"Body": notifications[req.params.notification]
-				}
-			},
-			function(error, httpResponse, body) {
-				if(error) { 
-					console.log(error);
-				} else if(body) { 
-					console.log(body); 
-				}
-			}
-		);
-	}
-	res.send(201);
-	next();
 }
 
-// Create server.
-var server = restify.createServer();
-
 // Configure server.
+var server = restify.createServer();
 server.use(restify.bodyParser({ mapParams: false }));
+server.use(restify.queryParser({ mapParams: false }));
+
+// Set routes.
 server.post('/notifications/:notification', notify);
 
 // Start server.
