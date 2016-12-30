a59be88868077067229e193e20eef69f9e68f37f
Nick Hall <nick@inck.net>
2016-12-30 18:06:00 -0500

    Implemented basic notifications resource, with date-base uids for aggregation. Cleaned up.

--- server.js
+++ server.js
@@ -1,18 +1,53 @@
 var restify = require('restify'),
 	request = require('request'),
-	twilioUrl = "https://api.twilio.com/2010-04-01/",
+	notifyToken = process.env.NOTIFY_TOKEN,
 	twilioAccount = process.env.TWILIO_ACCOUNT,
 	twilioNumber = process.env.TWILIO_NUMBER,
 	twilioToken = process.env.TWILIO_TOKEN,
-	notifyToken = process.env.NOTIFY_TOKEN,
+	twilioUrl = "https://api.twilio.com/2010-04-01/Accounts/" + twilioAccount,
 	notifications = {
-		"snow": "Calling all Snow Angels! Can you shovel today?"
+		"snow": "Calling all Snow Angels! Can you shovel today? Reply y/n."
 	};
+
+function getNotifications(req, res, next) {
+	request.get(
+		{
+			auth: {
+				"user": twilioAccount,
+				"pass": twilioToken
+			},
+			url: twilioUrl + '/Messages.json',
+		},
+		function(error, response, body) {
+			var notifications = JSON.parse(body).messages.map(function(notification){
+				var notificationDate = notification.body.match(/[0-9]{2}\/[0-9]{2}\/[0-9]{4}/i);
+				if(notificationDate != null){
+					notification["id"] = notificationDate[0];
+					return notification;
+				}
+			}).reduce(function(accumulated, current){
+				if(current != undefined) {
+					if(accumulated[current.id]) {
+						accumulated[current.id].push(current);
+					} else {
+						accumulated[current.id] = [current];
+					}
+				}
+				return accumulated;
+			}, {});
+
+			res.send(200, notifications);
+		}
+	);
+	next();
+}
 
 // Handle notification requests.
 function notify(req, res, next) {
 
-	var notification = notifications[req.params.notification]
+	var date = new Date();
+		notification = notifications[req.params.notification] + " (Sent " + (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear() + " by the City of Pittsburgh.)",
+		numbers = req.body.numbers;
 
 	// Check input.
 	if(!req.body) {
@@ -27,10 +62,10 @@ function notify(req, res, next) {
 	} else {
 
 		// For each number.
-		for(var property in req.body) {
+		for(var i=0; i < numbers.length; i++) {
 
-			// Standardize recipient number.  (Remove non-numbers.)  (Remove initial '1' if present.)
-			var recipient = req.body[property].replace(/[^0-9]/g, "").replace(/^1(.*)/g, "$1");
+			// Standardize recipient number.     (Remove non-numbers.)  (Remove any initial '1'.)
+			var recipient = numbers[i].toString().replace(/[^0-9]/g, "").replace(/^1(.*)/g, "$1");
 			console.log("Queueing message to " + recipient);
 
 			// Queue message.
@@ -39,15 +74,15 @@ function notify(req, res, next) {
 					auth: {
 						"user": twilioAccount,
 						"pass": twilioToken
-					},
-					url: twilioUrl + 'Accounts/' + twilioAccount + '/Messages.json',
+					}, 
+					url: twilioUrl + '/Messages.json',
 					form: {
 						"To": "+1" + recipient,
 						"From": twilioNumber,
 						"Body": notification
 					}
 				},
-				function(error, httpResponse, body) {
+				function(error, response, body) {
 					if(error) { 
 						console.log(error);
 					} else if(body) { 
@@ -58,7 +93,7 @@ function notify(req, res, next) {
 		}
 		res.send(201, { 
 			"notification": notification,
-			"numbers": req.body
+			"numbers": numbers
 		});
 		next();
 	}
@@ -68,11 +103,20 @@ function notify(req, res, next) {
 var server = restify.createServer();
 server.use(restify.bodyParser({ mapParams: false }));
 server.use(restify.queryParser({ mapParams: false }));
+server.use(
+	function crossOrigin(req,res,next){
+		res.header("Access-Control-Allow-Origin", "*");
+		res.header("Access-Control-Allow-Headers", "X-Requested-With");
+		return next();
+	}
+);
 
 // Set routes.
+server.get('/notifications', getNotifications);
+// server.get('/notifications/:notification', getNotification);
 server.post('/notifications/:notification', notify);
 
 // Start server.
 server.listen(process.env.NOTIFY_PORT, function() {
-	console.log('%s listening at %s', server.name, server.url);
+	console.log('Notify listening at %s.', process.env.NOTIFY_PORT);
 });
